e2125dfecae4a51b4b07b7f43f9f7545572471d9
RebeccaFuru <rebecca.furu@gmail.com>
2017-10-06 11:13:52 +0200

    min v1

--- BattleShipProj/Js/BattleJavaScript.js
+++ BattleShipProj/Js/BattleJavaScript.js
@@ -1,1 +1,26 @@
-﻿
+﻿$(document).ready(function () {
+
+
+    $("#loadGameButton").click(DrawGameBoard);
+
+
+
+
+}
+);
+
+function DrawGameBoard(){
+    var difficultyLevelForm = document.getElementById("difficultySelectorGroup");
+
+    var difficultyLevel = form.element["difficultySelector"].value; 
+    alert(difficultyLevel);
+    var size; 
+
+    //switch (difficultyLevel) {
+    //    case 
+    //}
+
+    //}
+    
+
+}
