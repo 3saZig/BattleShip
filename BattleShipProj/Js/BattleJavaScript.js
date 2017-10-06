d3b648b7f4b90dbd4bfb45726102cd0060267cc0
tobiasrondin <31711293+tobiasrondin@users.noreply.github.com>
2017-10-06 14:17:42 +0200

    wrapper

--- BattleShipProj/Js/BattleJavaScript.js
+++ BattleShipProj/Js/BattleJavaScript.js
@@ -15,10 +15,27 @@ function DrawGameBoard(){
 
     var size;
 
-    //switch (difficultyLevel) {
-    //    case:
-    //    default:
-    //}
-    alert(difficultyLevel);
+    switch (difficultyLevel) {
+        case 'easy': size = 5; break;
+        case 'medium': size = 10; break;
+        case 'hard': size = 15; break;
+        default: break;
+    }
+
+    document.getElementById("wrapper").style.height = (size * 100) + "px";
+    document.getElementById("wrapper").style.width = (size * 100) + "px";
+    
+
+    for (var i = 0; i < size; i++) {
+        //$("#gameBoard").append('<br>');
+
+        for (var j = 0; j < size; j++) {
+            $("#gameBoard").append('<div class="gameButton" id="' + i + j + '">hej</div>');
+        }
+    }
+    
+
+    
+
 
 }
