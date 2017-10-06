c127170bb5703b2789aa259af017fa1d4c47accf
RebeccaFuru <rebecca.furu@gmail.com>
2017-10-06 14:18:13 +0200

    hej

--- BattleShipProj/Js/BattleJavaScript.js
+++ BattleShipProj/Js/BattleJavaScript.js
@@ -15,10 +15,40 @@ function DrawGameBoard(){
 
     var size;
 
-    //switch (difficultyLevel) {
-    //    case:
-    //    default:
-    //}
+    switch (difficultyLevel) {
+
+        case 'easy':
+            size = 5;
+            break; 
+
+        case 'medium':
+            size = 10; 
+            break; 
+
+        case 'hard':
+            size = 15; 
+            break; 
+
+        default:
+    }
+
+    var gameBoardArray = new Array(size);
+
+    for (var i = 0; i < gameBoardArray.length; i++) {
+        gameBoardArray[i] = new Array(size); 
+    }
+
+    for (var i = 0; i < size .lengh; i++) {
+        $("#gameBoard").append('<br>');
+        for (var j = 0; j < gameBoardArray.length; j++) {
+            $("#gameBoard").append(i, j, '  '); 
+
+        }
+    }
+
+    
+
+
     alert(difficultyLevel);
 
 }
