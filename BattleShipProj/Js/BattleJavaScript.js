640ed96fdc42df58e14c2f0372edb918888e93ca
tobiasrondin <31711293+tobiasrondin@users.noreply.github.com>
2017-10-06 11:10:39 +0200

    Javascript

--- BattleShipProj/Js/BattleJavaScript.js
+++ BattleShipProj/Js/BattleJavaScript.js
@@ -1,1 +1,24 @@
-﻿
+﻿$(document).ready(function()
+{
+    $("#laddaKnappen").click(DrawGameBoard);
+
+
+
+}
+);
+
+function DrawGameBoard(){
+    var form = document.getElementById("difficultySelector");
+
+    var difficultyLevel = form.elements["difficulty"].value;
+
+
+    var size;
+
+    //switch (difficultyLevel) {
+    //    case:
+    //    default:
+    //}
+    alert(difficultyLevel);
+
+}
